f127e07d4e1f7cebda621f8fb359bfd8779a889f
Dilrabo Bobokalonova <dilidika@gmail.com>
2015-09-01 21:33:20 -0700

    first commit

---
+++ src/main/java/core/PrimeNumberCheck.java
@@ -0,0 +1,13 @@
+package core;
+
+public class PrimeNumberCheck {
+	public Boolean validate (final Integer primeNum){
+		for (int i = 2; i < (primeNum / 2); i++){
+			if (primeNum % i == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
